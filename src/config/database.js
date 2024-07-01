0e39dad75421eedd26a766e1b85271e1478e1db6
ffrontera <ffrontera@live.com>
2024-06-30 23:04:27 -0300

    variables de entorno

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PWD, {
-  host: DB_HOST,
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PWD, {
+  host: process.env.DB_HOST,
   dialect: "mysql",
 });
 
