be0bd28bd81a06a3ca95082441d11e817355bf1d
ffrontera <ffrontera@live.com>
2024-06-30 22:19:49 -0300

    se agrego modulo dotenv y variables de entorno

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("biblio", "root", "", {
-  host: "localhost",
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PWD, {
+  host: DB_HOST,
   dialect: "mysql",
 });
 
